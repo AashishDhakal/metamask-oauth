d9d2b3d084277744113d5ee12bf0a0b5e72fc1d3
AashishDhakal <aashish.dhakal@student.edu.np>
2021-05-28 23:27:14 +0545

    added current provider

--- index.js
+++ index.js
@@ -2,25 +2,20 @@ var ethUtil = require('ethereumjs-util')
 var sigUtil = require('eth-sig-util')
 var Eth = require('ethjs')
 var Web3 = require('web3')
-var web3 = new Web3(web3.currentProvider)
 window.Eth = Eth
 console.log('new V2')
 var fs = require('fs')
 var terms = fs.readFileSync(__dirname + '/terms.txt').toString()
 
-connectButton.addEventListener('click', function () {
-  connect()
-})
-
-function connect () {
-  if (typeof ethereum !== 'undefined') {
-    ethereum.enable()
-    .catch(console.error)
-  }
+if (typeof ethereum !== 'undefined') {
+  ethereum.enable()
+  var web3 = new Web3(Web3.currentProvider)
+  .catch(console.error)
 }
 
 ethSignButton.addEventListener('click', function(event) {
   event.preventDefault()
+  var web3 = new Web3(Web3.currentProvider)
   var original_message = 'An amazing message, for use with MetaMask!'
   var message_hash = web3.utils.sha3(
     '\u0019Ethereum Signed Message:\n' +
