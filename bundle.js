832d6de606ee92f9de644361c4f698c0a3fc57d5
AashishDhakal <aashish.dhakal@student.edu.np>
2021-05-28 22:36:02 +0545

    signature fix

--- bundle.js
+++ bundle.js
@@ -33,7 +33,7 @@ ethSignButton.addEventListener('click', async function(event) {
   console.log(msgHash)
   var from = '0xD7aFB34dbB1511Ce4a42BE0331A9e914A1cFd945'
   if (!from) return connect()
-  web3.eth.sign(from, msgHash, function (err, result) {
+  web3.eth.sign(msgHash, from, function (err, result) {
     if (err) return console.error(err)
     console.log('SIGNED:' + result)
   })
